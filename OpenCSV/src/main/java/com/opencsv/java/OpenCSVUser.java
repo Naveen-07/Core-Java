a2fcfab6d8a01a4e1e253d5427e8c1724391116e
Naveen-07 <naveennani0711@gmail.com>
2020-04-01 13:50:01 +0530

    [Naveen kumar]Added. Program for Open CSV Pojo Class

---
+++ OpenCSV/src/main/java/com/opencsv/java/OpenCSVUser.java
@@ -0,0 +1,56 @@
+package com.opencsv.java;
+
+public class OpenCSVUser {
+    private String name;
+    private String email;
+    private String phoneNo;
+    private String country;
+
+    public OpenCSVUser() {
+
+    }
+
+    public OpenCSVUser(String name, String email, String phoneNo, String country) {
+        this.name = name;
+        this.email = email;
+        this.phoneNo = phoneNo;
+        this.country = country;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name + "," + email + "," + phoneNo + "," + country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
